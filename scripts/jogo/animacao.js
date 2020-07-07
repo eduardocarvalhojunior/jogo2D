a9d42800e3e692d18c30daeba6a74c03d80eb866
eduardocarvalhojunior <junior.baller@hotmail.com>
2020-07-07 00:22:34 -0300

    inserindo inimigo1

---
+++ scripts/jogo/animacao.js
@@ -0,0 +1,32 @@
+class Animacao {
+    constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite){
+        this.matriz = matriz;
+        this.imagem = imagem;
+        this.x = x;
+        this.y = height - this.altura;
+        this.largura = largura;
+        this.altura = altura;
+        this.larguraSprite = larguraSprite;
+        this.alturaSprite = alturaSprite;
+
+        this.frameAtual = 0;
+    }
+
+    exibe(){
+        image(this.imagem, this.x, this.largura, this.altura, 
+            this.matriz[this.frameAtual][0], 
+            this.matriz[this.frameAtual][1],this.larguraSprite, this.alturaSprite);
+        
+        this.anima()
+      }
+
+      anima(){
+        this.frameAtual++
+        
+        if(this.frameAtual >= this.matriz.length - 1) {
+          this.frameAtual = 0
+        }
+      }
+
+
+}
---
+++ scripts/jogo/inimigo.js
@@ -0,0 +1,8 @@
+class Inimigo extends Animacao {
+    constructor(matriz, imagem, x, largura, 
+        altura, larguraSprite, alturaSprite){
+       
+        super(matriz, imagem, x, largura, 
+        altura, larguraSprite, alturaSprite);
+    }
+}
